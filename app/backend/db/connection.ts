3144ed417df383d599373b5546b241ad395f6f3b
bmarino <nolimit@bmarinos-MacBook-Pro.local>
2025-03-17 21:38:39 -0700

    mongo+more

---
+++ app/backend/db/connection.ts
@@ -0,0 +1,28 @@
+import {Collection, Db, MongoClient, ServerApiVersion} from 'mongodb';
+// @ts-ignore
+const DB_URI: string = process.env.MONGO_URI
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(DB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+export const database: Db = client.db('clothing')
+export const collections: Collection = database.collection('collections')
+export const inventory: Collection = database.collection('inventory')
+export const products: Collection = database.collection('products')
+export const productImages: Collection = database.collection('product-images')
+export const categories: Collection = database.collection('categories')
+
+export const connect = async () => {
+    try {
+        await client.connect()
+        console.log('connected!')
+    }
+    catch (err: unknown) {
+        console.error(err)
+    }
+}
+export default connect
